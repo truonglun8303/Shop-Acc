163515f451c33d78649ff8a5d488827817ac3be0
truong <quangtruongpham08032003@gmail.com>
2024-03-28 21:10:35 +0700

    shopacc

---
+++ ShopAcc/Controllers/MoreLOLController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using ShopAcc.Models;
+using ShopAcc.Models.EF;
+
+namespace ShopAcc.Controllers
+{
+    public class MoreLOLController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: MoreLOL
+        public ActionResult Index(int ? page)
+        {
+            var pageSize = 16;
+            if(page == null)
+            {
+                page = 1;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var item = db.Products.ToList();
+            return View(item.ToPagedList(pageIndex,pageSize));
+        }
+    }
+}
